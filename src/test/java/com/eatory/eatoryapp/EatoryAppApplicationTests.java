54f430cddc411c93d7becf051d8acdb2ca8bde21
Lewis Shum <wang.yin.lewis.shum@prudential.com.hk>
2023-05-16 03:49:03 +0800

    Initial commit

---
+++ src/test/java/com/eatory/eatoryapp/EatoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatory.eatoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
